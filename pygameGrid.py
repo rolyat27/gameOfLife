d6e6d740782d47a599b09b05b2bcf5f7054f11f2
Taylor Glaws <tglaws27@gmail.com>
2022-12-27 15:22:02 -0500

    This is a basic grid. next is logic for on/off states

---
+++ pygameGrid.py
@@ -0,0 +1,34 @@
+import pygame
+import sys
+
+BLACK = (0,0,0)
+WHITE = (200,200,200)
+HEIGHT = 600
+WIDTH = 600
+
+def main():
+    global SCREEN, CLOCK
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(BLACK)
+
+    while True:
+        drawGrid()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+
+def drawGrid():
+    blockSize = 20 #Set the size of the grid block
+    for x in range(0, WIDTH, blockSize):
+        for y in range(0, HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+main()
+
